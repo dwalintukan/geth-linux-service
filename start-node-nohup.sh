a50fc25d54ae0d36e8aa620dc79a469d0e978cb8
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-25 14:05:51 +0700

    Add start script

---
+++ start-node-nohup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Starts geth independently and sends it to the background.
+
+nohup \
+geth \
+--datadir "$DATA_DIR" \
+--syncmode full \
+--networkid "$CHAIN_ID" \
+--nat=none \
+--targetgaslimit 4700000 \
+--rpc \
+--rpcaddr "127.0.0.1" \
+--rpccorsdomain "127.0.0.1" \
+--verbosity 4 \
+--mine \
+--bootnodes "$BOOTNODES" \
+--etherbase "$ACCOUNT_ADDRESS" \
+--unlock "$ACCOUNT_ADDRESS" \
+--password "$PW_FILE" \
+&
